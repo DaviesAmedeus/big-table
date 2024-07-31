3cdeedcc19cce494621b2e8c0088a0c86cd61c87
DaviesAmedeus <amedeusedgar@gmail.com>
2024-07-31 23:15:02 +0300

    implemented the navigstion layout

---
+++ resources/views/components/navigation-links.blade.php
@@ -0,0 +1,39 @@
+<nav class="flex bg-gradient-to-r from-green-900 via-green-600 to-green-800 p-5 md:p-10">
+    
+    <div class="md:pl-10">
+      <div class="text-5xl font-bold md:text-6xl  md:font-bold">BiG <span class="font-outline-4">TABLE</span></div>
+      <div class="hidden text-center pt-1 tracking-widest md:block">We Make <span class="px-1">Contents</span> That Matter!</div>
+    </div>
+      
+      
+      <div class="flex items-center flex-1 justify-end gap-5 md:pr-10">
+
+        {{-- Search icon --}}
+       <div>
+        <div class="cursor-pointer">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="stroke-zinc-200 size-10">
+              <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+            </svg>
+          </div>
+       </div>
+
+        {{-- Menu button --}}
+        <div>
+            <div class="relative cursor-pointer">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-10">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" />
+                  </svg>
+            </div>
+
+            <div class="hidden absolute bg-gradient-to-r  font-bold pb-3 pt-3 text-center md:pt-0 from-green-900 via-green-600 to-green-800  w-full  left-0  justify-center gap-20  md:text-2xl md:flex  md:top-[168px]">
+                <div class="p-3 md:rounded-full  md:shadow-sm hover:bg-green-600 transition-colors ease-in ">Stories</div>
+                <div class="p-3 md:rounded-full md:shadow-sm hover:bg-green-600 transition-colors ease-in">Poetry</div>
+                <div class="p-3 md:rounded-full md:shadow-sm hover:bg-green-600 transition-colors ease-in">Videos</div>
+                <div class="p-3 md:rounded-full md:shadow-sm hover:bg-green-600 transition-colors ease-in">Others</div>
+            </div>
+          
+          
+        </div>
+      </div>
+
+</nav>
---
+++ resources/views/guest/index.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script defer src="./assets/fontawesome/js/solid.min.js"></script>
+  <script defer src="./assets/fontawesome/js/fontawesome.min.js"></script>
+  @vite('resources/css/app.css')
+</head>
+<body>
+  <header class="text-zinc-200">
+    <x-navigation-links />
+
+  </header>
+</body>
+</html>
